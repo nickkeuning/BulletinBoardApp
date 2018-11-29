3369c0b61b0b0b894d6a58e1b14008716b95b411
Nicholas Keuning <nkeuning@umich.edu>
2018-11-29 01:00:18 -0500

    Added ability to submit image to ocr endpoint

--- screens/ImagePickerScreen.js
+++ screens/ImagePickerScreen.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { Alert, ScrollView, View, Platform, Image, Button, Text } from 'react-native';
-import { ImagePicker, Permissions, Video, Constants } from 'expo';
-import { NavigationEvents } from 'react-navigation';
+import React from "react";
+import {
+  Alert,
+  ScrollView,
+  View,
+  Platform,
+  Image,
+  Button,
+  Text
+} from "react-native";
+import { ImagePicker, Permissions, Video, Constants } from "expo";
+import { NavigationEvents } from "react-navigation";
+import MonoText from "../components/MonoText"
 
 export default class ImagePickerScreen extends React.Component {
   static navigationOptions = {
-    title: 'Submit Poll Tape',
+    title: "Submit Poll Tape"
   };
   state = {
     selection: null,
+    ocrResult: null
   };
 
   async componentDidFocus() {
@@ -25,56 +35,86 @@ export default class ImagePickerScreen extends React.Component {
       }
     };
 
-    //   const { serverAddress } = Constants.manifest.extra;
+    const { selection } = this.state;
+    const selectionRender =
+      !selection ? (selection) : (this.renderSelection(selection));
+
+    const submitButton = 
+      !selection ? (selection) : (this.renderSubmitButton());
 
     return (
       <ScrollView style={{ padding: 10 }}>
         <NavigationEvents onDidFocus={this.componentDidFocus} />
         <Button onPress={showCamera} title="Take Photo of Poll Tape" />
-        {this._maybeRenderSelection()}
+        {selectionRender}
+        {submitButton}
       </ScrollView>
     );
   }
 
-  _maybeRenderSelection = () => {
-    const { selection } = this.state;
-
-    if (!selection) {
-      return;
-    }
-
-    const media =
-      selection.type === 'video' ? (
-        <Video
-          source={{ uri: selection.uri }}
-          style={{ width: 300, height: 300 }}
-          resizeMode="contain"
-          shouldPlay
-          isLooping
-        />
-      ) : (
-        <Image
-          source={{ uri: selection.uri }}
-          style={{ width: 300, height: 300, resizeMode: 'contain' }}
-        />
-      );
-
-      const result = <Text>{JSON.stringify(selection, null, 2)}</Text>;
-
+  renderSelection(selection) {
     return (
       <View style={{ marginVertical: 16 }}>
         <View
           style={{
             marginBottom: 10,
-            alignItems: 'center',
-            justifyContent: 'center',
+            alignItems: "center",
+            justifyContent: "center",
             flex: 1,
-            backgroundColor: '#000000',
-          }}>
-          {media}
+            backgroundColor: "#000000"
+          }}
+        >
+          <Image
+            source={{ uri: selection.uri }}
+            style={{ width: 300, height: 300, resizeMode: "contain" }}
+          />
         </View>
-        {result}
       </View>
     );
-  };
+  }
+
+  renderSubmitButton() {
+    const submitImage = async () => {
+      Constants.manifest.extra.ocrEndpoint = "/api/ocr";
+      const { serverAddress, serverPort, ocrEndpoint } = Constants.manifest.extra;
+      const requestAddress = `http://${serverAddress}:${serverPort}${ocrEndpoint}`;
+      const { selection } = this.state;
+
+      let formData = new FormData();
+      let photo = {
+        uri: selection.uri,
+        type: "image/jpg",
+        name: "image.jpg"
+      }
+      formData.append("image", photo);
+
+      fetch(requestAddress, {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "multipart/form-data"
+        },
+        body: formData
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({ocrResult: responseJson});
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    };
+
+    const ocrResultRender =
+      !this.state.ocrResult ? (this.state.ocrResult) : (
+        <MonoText>{this.state.ocrResult.output}</MonoText>
+      );
+
+    return (
+      <View>
+        <Button onPress={submitImage} title="Submit Poll Tape" />
+        {ocrResultRender}
+      </View>
+    );
+  }
 }
